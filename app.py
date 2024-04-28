102d4681dbe403949aee1909f36b786d47c51f87
jenviera <165118442+jenviera@users.noreply.github.com>
2024-04-27 22:02:16 -0400

    checkbox

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import plotly.express as px
 import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
+build_histogram = st.checkbox('Construir un histograma')
 
-if hist_button: # al hacer clic en el botón
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
+
     # escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
             
@@ -17,7 +19,6 @@ if hist_button: # al hacer clic en el botón
         
 
 st.header('Proyecto Sprint 4: Software Development Tools')
-st.write('Prueba')
 
 # Agregar un párrafo debajo del encabezado
 st.write("Este es un análisis de datos de vehículos. Puedes seleccionar dos tipos de visualizaciones a continuación.")
